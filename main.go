038708af66bd4cfcae657339cc2d92217ac5c3c2
Anders Kvist <ak@cego.dk>
2019-07-04 21:22:46 +0200

    Added webserver to handle the conversion - needs cleanup.

--- main.go
+++ main.go
@@ -1,17 +1,104 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strconv"
+	"time"
+
 	"gopkg.in/gographics/imagick.v2/imagick"
 
 	"github.com/anderskvist/GoHelpers/log"
 	"github.com/anderskvist/GoHelpers/version"
 )
 
+func uploadFile(w http.ResponseWriter, r *http.Request) {
+	log.Info("File Upload Endpoint Hit")
+
+	// Parse our multipart form, 10 << 20 specifies a maximum
+	// upload of 10 MB files.
+	r.ParseMultipartForm(10 << 20)
+
+	// FormFile returns the first file for the given key `myFile`
+	// it also returns the FileHeader so we can get the Filename,
+	// the Header and the size of the file
+	file, handler, err := r.FormFile("PDF")
+	if err != nil {
+		log.Error("Error Retrieving the File")
+		log.Error(err)
+		return
+	}
+	defer file.Close()
+	log.Debugf("Uploaded File: %+v\n", handler.Filename)
+	log.Debugf("File Size: %+v\n", handler.Size)
+	log.Debugf("MIME Header: %+v\n", handler.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile("/tmp/", "upload-*")
+	if err != nil {
+		log.Error(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(file)
+	if err != nil {
+		log.Error(err)
+	}
+
+	// write this byte array to our temporary file
+	_, err = tempFile.Write(fileBytes)
+	if err != nil {
+		log.Error(err)
+	} else {
+		if err := ConvertPdfToJpg(tempFile.Name(), "/tmp/testing.png"); err != nil {
+			log.Fatal(err)
+		}
+
+		downloadBytes, err := ioutil.ReadFile("/tmp/testing.png")
+
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		// set the default MIME type to send
+		mime := http.DetectContentType(downloadBytes)
+
+		fileSize := len(string(downloadBytes))
+
+		w.Header().Set("Content-Type", mime)
+		w.Header().Set("Content-Disposition", "attachment; filename=pdf.png")
+		w.Header().Set("Expires", "0")
+		w.Header().Set("Content-Transfer-Encoding", "binary")
+		w.Header().Set("Content-Length", strconv.Itoa(fileSize))
+		w.Header().Set("Content-Control", "private, no-transform, no-store, must-revalidate")
+
+		// force it down the client's.....
+		http.ServeContent(w, r, "/tmp/testing.png", time.Now(), bytes.NewReader(downloadBytes))
+
+	}
+
+	// return that we have successfully uploaded our file!
+	fmt.Fprintf(w, "Successfully Uploaded File\n")
+	http.Redirect(w, r, "/", 301)
+
+}
+
 func main() {
 	log.Infof("GoPDF2PNG version: %s.\n", version.Version)
 
 	pdfName := "test.pdf"
 	imageName := "test.jpg"
+
+	fs := http.FileServer(http.Dir("html"))
+	http.Handle("/", fs)
+
+	http.HandleFunc("/upload", uploadFile)
+	log.Fatal(http.ListenAndServe(":80", nil))
 
 	if err := ConvertPdfToJpg(pdfName, imageName); err != nil {
 		log.Fatal(err)
